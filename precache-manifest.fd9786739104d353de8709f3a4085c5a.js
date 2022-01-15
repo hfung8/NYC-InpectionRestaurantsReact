d194ebb9ab0858516fce69ad6f99980dd2ddbdf0
Harrison Fung <harrison.hok.fung@gmail.com>
2022-01-15 17:24:11 -0500

    Updates

---
+++ precache-manifest.fd9786739104d353de8709f3a4085c5a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946bcaeac7a45432b0b0872c0416b0f2",
+    "url": "/hfportfolio/index.html"
+  },
+  {
+    "revision": "9ad7ae5f6e7377a7a258",
+    "url": "/hfportfolio/static/css/2.67731326.chunk.css"
+  },
+  {
+    "revision": "9ef0d56ded97731cff9d",
+    "url": "/hfportfolio/static/css/main.e8051ee4.chunk.css"
+  },
+  {
+    "revision": "9ad7ae5f6e7377a7a258",
+    "url": "/hfportfolio/static/js/2.6987c6a1.chunk.js"
+  },
+  {
+    "revision": "9ef0d56ded97731cff9d",
+    "url": "/hfportfolio/static/js/main.573fa5dc.chunk.js"
+  },
+  {
+    "revision": "5f3d902c66a06e32d5fe",
+    "url": "/hfportfolio/static/js/runtime-main.d8f7cfb8.js"
+  },
+  {
+    "revision": "c17186cc5a1ab764c24077c99abb9d18",
+    "url": "/hfportfolio/static/media/7seconds.c17186cc.png"
+  },
+  {
+    "revision": "c938d0a3afb11b1b7d9dd4e5fd4d5ac8",
+    "url": "/hfportfolio/static/media/Card_game.c938d0a3.png"
+  },
+  {
+    "revision": "fdd703b356dd334fa2fbbe034202e9ec",
+    "url": "/hfportfolio/static/media/OHU.fdd703b3.png"
+  },
+  {
+    "revision": "74aa02f2091f426199c6a4caa70a2f1d",
+    "url": "/hfportfolio/static/media/harrycircle.74aa02f2.png"
+  }
+]);
